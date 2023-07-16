40eb1a81de54f4f7b51c14ba15404bd6f1d20f12
av.bessmelcev <av.bessmelcev@tenzor.ru>
2023-07-16 22:02:55 +0700

    added logic to sign-up page

--- pages/sign-up/index.vue
+++ pages/sign-up/index.vue
@@ -1,37 +1,61 @@
 <template>
-  <div class="content content_sign-in">
-    <p class="content__title">{{$t('titles.registration')}}</p>
-    <div v-for="(inputData) in registryInputs">
-      <base-field
-        v-model="model[inputData.type]"
-        :ref="inputData.type"
-        :name="$tc(`signUpOrIn.` + inputData.type)"
-        :rules="inputData.rules"
-        :label="$tc(`signUpOrIn.` + inputData.type)"
-        mode="icon"
-        placeholder="-"
-        data-selector="inputData.dataSelector"/>
-    </div>
-    <div class="content__info">
-      {{$t('signUpOrIn.additionalInfoPartOne')}}
-      <a class="content__info-link">{{$t('signUpOrIn.termsConditions')}}</a>
-      {{$t('signUpOrIn.additionalInfoPartTwo')}}
-      <a>{{$t('signUpOrIn.privacyPolicy')}}</a>
-    </div>
-    <base-btn
-      @click="toSignInFunc"
-      mode="active"
-    >
-      {{$t('buttons.enter')}}
-    </base-btn>
-    <div class="content__links">
-      <div class="content__text">{{$t('signUpOrIn.haveAccount')}}</div>
-      <nuxt-link
-        class="content__link"
-        to="/sign-in">
-        {{$t('signUpOrIn.login')}}
-      </nuxt-link>
-    </div>
+  <div>
+    <template v-if="step===0">
+      <div class="content content_sign-in">
+        <p class="content__title">{{$t('titles.registration')}}</p>
+        <div v-for="(inputData) in registryInputs">
+          <base-field
+            v-model="model[inputData.type]"
+            :ref="inputData.type"
+            :name="$tc(`signUpOrIn.` + inputData.type)"
+            :rules="inputData.rules"
+            :label="$tc(`signUpOrIn.` + inputData.type)"
+            mode="icon"
+            placeholder="-"
+            data-selector="inputData.dataSelector"/>
+        </div>
+        <div class="content__info">
+          {{$t('signUpOrIn.additionalInfoPartOne')}}
+          <a class="content__info-link">{{$t('signUpOrIn.termsConditions')}}</a>
+          {{$t('signUpOrIn.additionalInfoPartTwo')}}
+          <a>{{$t('signUpOrIn.privacyPolicy')}}</a>
+        </div>
+        <base-btn
+          @click="toConfirmCode"
+          mode="active"
+        >
+          {{$t('buttons.enter')}}
+        </base-btn>
+        <div class="content__links">
+          <div class="content__text">{{$t('signUpOrIn.haveAccount')}}</div>
+          <nuxt-link
+            class="content__link"
+            to="/sign-in">
+            {{$t('signUpOrIn.login')}}
+          </nuxt-link>
+        </div>
+      </div>
+    </template>
+    <template v-else>
+      <div class="content content_sign-in">
+        <p class="content__title">{{$t('titles.registration')}}</p>
+        <base-field
+            v-model="code"
+            ref="code"
+            :name="$tc('signUpOrIn.code')"
+            rules="required_if|alpha_spaces_dash|max:8"
+            :label="$tc('signUpOrIn.enterCode')"
+            mode="icon"
+            placeholder="-"
+            data-selector="inputData.dataSelector"/>
+        <base-btn
+            @click="toSignInFunc"
+            mode="active"
+        >
+          {{$t('buttons.enter')}}
+        </base-btn>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -46,12 +70,12 @@ export default {
       registryInputs: [
         {
           type: 'userName',
-          rules: 'required_if|alpha_spaces_dash|max:15',
+          rules: 'required_if|max:15',
           dataSelector: 'FIRST_NAME'
         },
         {
-          type: 'linkToProfile',
-          rules: 'required|email',
+          type: 'alias',
+          rules: 'required',
           dataSelector: 'LOGIN'
         },
         {
@@ -66,22 +90,40 @@ export default {
         },
         {
           type: 'passwordConfirm',
-          rules: 'required_if|min:8|confirmed:confirmation',
+          rules: 'required_if|min:8',
           dataSelector: 'CONFIRM_PASSWORD'
         }
       ],
       model: {
-        firstName: '',
-        lastName: '',
+        userName: '',
+        alias: '',
         email: '',
         password: '',
         passwordConfirm: '',
-      }
+      },
+      code: '',
+      step: 0,
     };
   },
   methods: {
-    toSignInFunc() {
-      this.$router.push(this.toSignIn);
+    async toConfirmCode() {
+      const payload = {
+        email: this.model.email,
+        password: this.model.password,
+        name: this.model.userName,
+        alias: this.model.alias
+      }
+      const result = await this.$store.dispatch('sign-up/registration', payload);
+      console.log(result);
+      if (result !== false) {
+        this.step = 1;
+      }
+    },
+    async toSignInFunc() {
+      const result = await this.$store.dispatch('sign-up/confirmRegistration', {});
+      if (result) {
+        await this.$router.push(this.toSignIn);
+      }
     }
   }
 }
--- store/sign-up/actions.js
+++ store/sign-up/actions.js
@@ -1,3 +1,18 @@
 export default {
-
+    async registration({commit}, payload) {
+        try {
+            return await this.$axios.$post('user/registration', payload);
+        } catch (err) {
+            console.error(err);
+            return false;
+        }
+    },
+    async confirmRegistration({commit}, payload) {
+        try {
+            return await this.$axios.$post('user/registration/confirm', payload);
+        } catch (err) {
+            console.error(err);
+            return false;
+        }
+    }
 }
